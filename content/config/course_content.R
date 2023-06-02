a7d7f3bb548dca62a0bb7ef26cc2ea2e0f41c07c
Jünger <stefan.juenger@gesis.org>
2023-06-02 12:03:48 +0200

    short update

--- content/config/course_content.R
+++ content/config/course_content.R
@@ -1,16 +1,16 @@
 course_content <-
   tibble::tribble(
     ~Day, ~Time, ~Title,
-    "July 14", "09:00am-10:30am", "Introduction to GIS",
-    "July 14", "10:30am-12:00pm", "Vector Data",
-    "July 14", "12:00pm-01:00pm", "Lunch Break",
-    "July 14", "01:00pm-02:30pm", "Mapping",
-    "July 14", "02:30pm-04:00pm", "Raster Data",
-    "July 15", "09:00am-10:00am", "Advanced Data Import & Processing",
-    "July 15", "10:00am-11:00am", "Applied Data Wrangling & Linking",
-    "July 15", "11:00am-12:00pm", "Investigating Spatial Autocorrelation",
-    "July 15", "12:00pm-01:00pm", "Lunch Break",
-    "July 15", "01:00pm-02:45pm", "Spatial Econometrics & Outlook"
+    "June 06", "10:00-11:30", "Introduction to GIS",
+    "June 06", "11:45-13:00", "Vector Data",
+    "June 06", "13:00-14:00", "Fingerfood@GESIS",
+    "June 06", "14:00-15:30", "Mapping",
+    "June 06", "15:45-17:00", "Raster Data",
+    "June 07", "09:00-10:30", "Advanced Data Import & Processing",
+    "June 07", "10:45-12:00", "Applied Data Wrangling & Linking",
+    "June 07", "12:00-13:00", "Lunch Break",
+    "June 07", "13:00-14:30", "Investigating Spatial Autocorrelation",
+    "June 07", "14:45-16:00", "Spatial Econometrics & Outlook"
   ) %>%
   knitr::kable() %>%
   kableExtra::kable_styling() %>%
@@ -18,5 +18,5 @@ course_content <-
   kableExtra::column_spec(2, color = "gray") %>%
   kableExtra::column_spec(3, bold = TRUE) %>%
   kableExtra::row_spec(3, color = "gray") %>%
-  kableExtra::row_spec(9, color = "gray") %>%
+  kableExtra::row_spec(8, color = "gray") %>%
   kableExtra::row_spec(5, extra_css = "border-bottom: 1px solid")
